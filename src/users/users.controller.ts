6377a9dfd0b845d5044e30fcace61bd192bece15
Cheena <c.bazarbai@gmail.com>
2024-08-15 16:40:07 +0500

    feat: add jwt authentication to /api/v1/users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,31 +7,37 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JWTGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('api/v1/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseGuards(JWTGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @UseGuards(JWTGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JWTGuard)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(JWTGuard)
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -40,6 +46,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @UseGuards(JWTGuard)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
   }
